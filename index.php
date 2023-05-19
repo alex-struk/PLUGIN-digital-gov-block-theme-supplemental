881470635c715dc2e900fa678986c702a8288157
Struk <Alex.Struk@gov.bc.ca>
2023-05-19 16:07:31 -0700

    ability to view vue app in block editor and configure it's settings, styling changes

--- index.php
+++ index.php
@@ -32,11 +32,12 @@ function custom_assets_loader() {
             $file_url = plugins_url(str_replace($plugin_dir, '', $file), __FILE__);
             wp_enqueue_style('custom-public-' . basename($file, '.css'), $file_url);
         }
-
-        foreach ($public_js_files as $file) {
-            $file_url = plugins_url(str_replace($plugin_dir, '', $file), __FILE__);
-            wp_enqueue_script('custom-public-' . basename($file, '.js'), $file_url, [], false, true);
-        }
+        
+        // todo: disabled public js loading because it conflicts with Vue app (variable k was already defined)
+        // foreach ($public_js_files as $file) {
+        //     $file_url = plugins_url(str_replace($plugin_dir, '', $file), __FILE__);
+        //     wp_enqueue_script('custom-public-' . basename($file, '.js'), $file_url, [], false, true);
+        // }
     } else {
         // Load admin CSS and JS files
         foreach ($admin_css_files as $file) {
@@ -52,3 +53,70 @@ function custom_assets_loader() {
 }
 add_action('wp_enqueue_scripts', 'custom_assets_loader');
 add_action('admin_enqueue_scripts', 'custom_assets_loader');
+
+// Register block to load the Vue.js app
+function vuejs_wordpress_block() {
+    wp_enqueue_script(
+        'vuejs-wordpress-block',
+        plugin_dir_url(__FILE__) . 'block.js',
+        array( 'wp-blocks', 'wp-element', 'wp-editor' )
+    );
+}
+
+add_action( 'enqueue_block_editor_assets', 'vuejs_wordpress_block' );
+
+// this loads vue app assets onto the client: todo: this happens for all pages, not just when the block is used
+function vuejs_app() {
+    $plugin_dir = plugin_dir_path(__FILE__);
+    $assets_dir = $plugin_dir . 'blocks/wcag-vue-app/wcag-app/dist/assets/';
+    $public_js_files = glob($assets_dir . 'index-*.js');
+    $public_css_files = glob($assets_dir . 'index-*.css');
+
+    if (is_admin()) {
+        foreach ($public_css_files as $file) {
+            $file_url = plugins_url(str_replace($plugin_dir, '', $file), __FILE__);
+            wp_enqueue_style('vue-app-' . basename($file, '.css'), $file_url);
+        }
+
+        foreach ($public_js_files as $file) {
+            $file_url = plugins_url(str_replace($plugin_dir, '', $file), __FILE__);
+            wp_enqueue_script('vue-app-' . basename($file, '.js'), $file_url, [], false, true);
+        }
+    }
+}
+
+add_action( 'enqueue_block_editor_assets', 'vuejs_app' );
+
+// load vue app assets, only when the block is used on the page
+function vuejs_app_dynamic_block( $attributes ) {
+    $plugin_dir = plugin_dir_path( __FILE__ );
+    $assets_dir = $plugin_dir . 'blocks/wcag-vue-app/wcag-app/dist/assets/';
+    $public_js_files = glob($assets_dir . 'index-*.js');
+    $public_css_files = glob($assets_dir . 'index-*.css');
+
+    foreach ($public_css_files as $file) {
+        $file_url = plugins_url(str_replace($plugin_dir, '', $file), __FILE__);
+        wp_enqueue_style('vue-app-' . basename($file, '.css'), $file_url);
+    }
+
+    foreach ($public_js_files as $file) {
+        $file_url = plugins_url(str_replace($plugin_dir, '', $file), __FILE__);
+        wp_enqueue_script('vue-app-' . basename($file, '.js'), $file_url, [], false, true);
+    }
+
+     // Access the 'columns' attribute
+     $columns = isset( $attributes['columns'] ) ? $attributes['columns'] : 3;  // Fallback to '3' if not set
+
+     // Access the 'className' attribute
+     $className = isset( $attributes['className'] ) ? $attributes['className'] : ''; 
+
+     // Add the 'data-columns' attribute to the output div
+     return '<div id="app" class="' . esc_attr( $className ) . '" data-columns="' . esc_attr( $columns ) . '">Loading...</div>';
+}
+
+function vuejs_app_block_init() {
+    register_block_type( 'my-plugin/vuejs-wordpress-block', [
+        'render_callback' => 'vuejs_app_dynamic_block',
+    ] );
+}
+add_action( 'init', 'vuejs_app_block_init' );
